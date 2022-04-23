057a6f9ad839b11b181c57c629232ea21d3561f0
karolnancka <karolnancka@gmail.com>
2022-04-23 16:44:54 +0200

    Spring Java - Task 02

--- src/main/java/pl/coderslab/spring/AppConfig.java
+++ src/main/java/pl/coderslab/spring/AppConfig.java
@@ -1,8 +1,13 @@
 package pl.coderslab.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.coderslab.beans.HelloWorld;
 
 @Configuration
 public class AppConfig {
-
+    @Bean
+    public HelloWorld helloWorld(){
+        return new HelloWorld();
+    }
 }
--- src/main/java/pl/coderslab/spring/SpringDiApplication.java
+++ src/main/java/pl/coderslab/spring/SpringDiApplication.java
@@ -1,10 +1,13 @@
 package pl.coderslab.spring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pl.coderslab.beans.HelloWorld;
 
 public class SpringDiApplication {
     public static void main(String[] args) {
        AnnotationConfigApplicationContext ctx =  new AnnotationConfigApplicationContext(AppConfig.class);
+       HelloWorld hw = ctx.getBean("helloWorld", HelloWorld.class);
+       hw.hello();
        ctx.close();
     }
 }
